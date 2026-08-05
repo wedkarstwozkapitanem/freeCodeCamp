59628480840ebf508217417af7b0384d4dd29aa6
Dominik Łempicki Kapitan <dominiklempicki@gmail.com>
2026-08-05 12:38:34 +0200

    Add FreeCodeCamp daily solution 05.08.2026 (python)

--- daily/2026/2026-08-05/solution.py
+++ daily/2026/2026-08-05/solution.py
@@ -7,19 +7,31 @@
 """
 
 
-import math
-
 def get_spoken_duration(seconds):
-    hour = math.floor(seconds / 3600)
-    minuts = math.floor((seconds - (hour * 3600)) / 60)
-    seconds = seconds - (hour * 3600) - (minuts * 60)
-    h = "hours" if hour != 1 else "hour"
-    m = "minutes" if minuts != 1 else "minute"
-    s = "seconds" if seconds != 1  else "second"
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
 
-    return (str(hour) + " " + h + (", " if minuts and seconds else "") if hour > 0 else "") + (str(minuts) + " " + m if minuts > 0 else "") + (" and " if (hour and seconds) or (minuts and seconds) or (hour and minuts) else "") + ((str(seconds) + " " + s) if seconds > 0 or (hour == 0 and minuts == 0) else "")
+    wynik = list()
+
+    if hours:
+        wynik.append(f'{hours} {"hours" if hours != 1 else "hour"}')
+    
+    if minutes:
+        wynik.append(f'{minutes} {"minutes" if minutes != 1 else "minute"}')
+
+    if seconds:
+        wynik.append(f'{seconds} {"seconds" if seconds != 1  else "second"}')
+    if not wynik:
+        return "0 seconds"
+    elif len(wynik) == 1:
+        return wynik[0]
+    elif len(wynik) == 2:
+        return " and ".join(wynik)
+
+    return wynik[0] + ", " + wynik[1] + " and " + wynik[2]
    
-    return  h +  + str(minuts) + " " + m + " and " + str(seconds) + " " + s  
+    
 
 print(get_spoken_duration(3723))
 print(get_spoken_duration(7295))
