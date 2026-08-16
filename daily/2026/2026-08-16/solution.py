280d9ddfa23e72058b06c48cdef29591145608f9
Dominik Łempicki Kapitan <dominiklempicki@gmail.com>
2026-08-16 15:14:24 +0200

    Add solution FreeCodeCamp daily challenge

---
+++ daily/2026/2026-08-16/solution.py
@@ -0,0 +1,12 @@
+"""
+  ==========================================
+  || FreeCodeCamp – Daily Coding Challenge ||
+  || Date: 2026-08-16                      ||
+  || Dominik Łempicki (kapitan)            ||
+  ==========================================
+"""
+
+def are_anagrams(str1, str2):
+    str1 = sorted("".join(i.lower() for i in str1 if i != " "))
+    str2 = sorted("".join(i.lower() for i in str2 if i != " "))
+    return str1 == str2
