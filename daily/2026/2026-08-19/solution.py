4883925e0af8c37d567d47d785e71c651e6cf92c
Dominik Łempicki Kapitan <dominiklempicki@gmail.com>
2026-08-19 11:57:33 +0200

    Add FreeCodeCamp daily coding solution

---
+++ daily/2026/2026-08-19/solution.py
@@ -0,0 +1,10 @@
+"""
+  ==========================================
+  || FreeCodeCamp – Daily Coding Challenge ||
+  || Date: 2026-08-19                      ||
+  || Dominik Łempicki (kapitan)            ||
+  ==========================================
+"""
+
+def sum_of_squares(n):
+    return sum(i * i for i in range(n + 1))
