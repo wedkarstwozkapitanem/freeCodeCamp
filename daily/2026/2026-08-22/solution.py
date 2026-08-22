131258f92be62c9200c9e3c31e28a447d76d291d
Dominik Łempicki Kapitan <dominiklempicki@gmail.com>
2026-08-22 19:51:55 +0200

    Add FreeCodeCamp daily solution

---
+++ daily/2026/2026-08-22/solution.py
@@ -0,0 +1,19 @@
+"""
+  ==========================================
+  || FreeCodeCamp – Daily Coding Challenge ||
+  || Date: 2026-08-20                      ||
+  || Dominik Łempicki (kapitan)            ||
+  ==========================================
+"""
+
+def decode(message, shift):
+    shift %= 26
+    return "".join(
+        chr((ord(i) - shift - 65) % 26 + 65) if i.isupper() 
+        else chr((ord(i) - shift - 97) % 26 + 97) if i.islower() 
+        else i
+        for i in message
+        )
+
+print(decode("Xlmw mw e wigvix qiwweki.", 4))
+print( decode("Byffi Qilfx!", 20))
